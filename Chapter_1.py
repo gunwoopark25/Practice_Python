2b72e2ca51423170f1f389653e66188105112a25
DESKTOP-UBQLVLG\Geonwoo <gunwoo0122@mail.ulsan.ac.kr>
2026-07-22 17:10:22 +0900

    공부중

--- Chapter_1.py
+++ Chapter_1.py
@@ -6,4 +6,4 @@ result = First_number + Second_number
 
 print(str(First_number) + "와 " + str(Second_number) + "를 더하면 " + str(result) + "입니다.")
 
-# input data를 int로 넣었는데 print할 때, 문자열과 같이 추출을 해야하기 때문에 str로 감싸서 문자열로 변환해줘서 출력 
+# input data를 int로 넣었는데 print할 때, 문자열과 같이 추출을 해야하기 때문에 str로 감싸서 문자열로 변환해줘서 출력
