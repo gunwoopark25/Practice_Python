d7e223c7e462d3dc7752af2dfa3d3a03f47198cf
DESKTOP-UBQLVLG\Geonwoo <gunwoo0122@mail.ulsan.ac.kr>
2026-07-22 16:45:21 +0900

    first commit

---
+++ Chapter_1.py
@@ -0,0 +1,9 @@
+# calculate
+First_number = int(input("덧셈을 할 첫번째 숫자 : "))
+Second_number = int(input("덧셈을 할 두번째 숫자 : "))
+
+result = First_number + Second_number
+
+print(str(First_number) + "와 " + str(Second_number) + "를 더하면 " + str(result) + "입니다.")
+
+# input data를 int로 넣었는데 print할 때, 문자열과 같이 추출을 해야하기 때문에 str로 감싸서 문자열로 변환해줘서 출력 
